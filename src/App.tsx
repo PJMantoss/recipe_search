07cee33cb40221405d21efca3583f82f9a8ee6ae
PJMantoss <joelptoss@gmail.com>
2021-05-30 18:29:22 +0100

    add Irecipe.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React, { FormEvent, useEffect, useState } from 'react';
+import { IRecipe } from './IRecipe';
 import './App.css';
 
 function App() {
-  const [recipeFound, setRecipeFound] = useState([]);
+  const [recipeFound, setRecipeFound] = useState<IRecipe[]>([]);
   const [recipeSearch, setRecipeSearch] = useState("");
 
-  const searchForRecipes = async (query: string): Promise<any>  => {
+  const searchForRecipes = async (query: string): Promise<IRecipe[]>  => {
     const result = await fetch(`http://localhost:3000/?search=${query}`);
     return (await result.json()).results;
   }
